c2c99fa23093ac35620880545bb133a9cf1dddb2
Erick Mendonça <erickmendonca@yahoo.com.br>
2015-01-05 16:29:06 -0300

    Adds show on map menu option

--- app/src/main/java/br/com/gdgaracaju/sunshine/app/MainActivity.java
+++ app/src/main/java/br/com/gdgaracaju/sunshine/app/MainActivity.java
@@ -2,9 +2,19 @@ package br.com.gdgaracaju.sunshine.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -38,13 +48,49 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingsIntent);
+        switch (id) {
+            case (R.id.action_settings):
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
+            case (R.id.action_show_map):
+                showPreferredLocationOnMap();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showPreferredLocationOnMap() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        final String location = sharedPref.getString(this.getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        final Geocoder geocoder = new Geocoder(this);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(location, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                // Use the address as needed
+                showMap(address);
+            } else {
+                // Display appropriate message when Geocoder services are not available
+                Toast.makeText(this, getString(R.string.error_geocode_zip), Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            // handle exception
+            Log.e(LOG_TAG, getString(R.string.error_geolocation),e);
+            e.printStackTrace();
+        }
+    }
+
+    private void showMap(Address address) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(address.getUrl()));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
